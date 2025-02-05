01ce34a6664c8c803c3a1fb31ae7f2507f7ce7aa
kuaidy <732634939@qq.com>
2025-02-05 20:13:27 +0800

    重构

---
+++ XzNav/XzNav/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace XzNav.Model
+{
+    public class CategoryModel
+    {
+        public string EName { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string File { get; set; }
+        public string CatUrl { get; set; }
+        public List<CategoryModel> Children { get; set; }
+        public List<SiteModel> Sites { get; set; } = new List<SiteModel>();
+    }
+}
---
+++ XzNav/XzNav/Model/Site.cs
@@ -0,0 +1,18 @@
+﻿namespace XzNav.Model
+{
+    public class SiteModel
+    {
+        public string Name { get; set; }
+        public string SiteUrl { get; set; }
+
+        public string Icon
+        {
+            get { return $"Images/{Name}.svg"; }
+        }
+        public string OnlineIcon
+        {
+            get; set;
+        }
+
+    }
+}
